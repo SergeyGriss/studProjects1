9d83d620a7c18d80edf4f00d3ffe78f929c666e8
SergeyGriss <SergeyGriss@users.noreply.github.com>
2020-10-14 00:37:36 +0300

    UnitTestProjectAdded

---
+++ DeveloperInstrumentalTools-master/Examples/Testing/UnitTests/DayOfWeekServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using UnitTestsTarget;
+
+namespace UnitTests
+{
+    class DayOfWeekServiceTests
+    {
+        [Test]
+        public void GetWeekend()
+        {
+            // Arrange
+            var thisSunday = new DateTime(2020, 10, 04);
+
+            var service = new DayOfWeekService();
+
+            // Act
+            var result = service.IsWeekend(thisSunday);
+
+            // Assert
+            result.Should().Be(true);
+        }
+        [Test]
+        public void GetNotWeekend()
+        {
+            // Arrange
+            var thisSunday = new DateTime(2020, 10, 05);
+
+            var service = new DayOfWeekService();
+
+            // Act
+            var result = service.IsWeekend(thisSunday);
+
+            // Assert
+            result.Should().Be(false);
+        }
+    }
+}
---
+++ DeveloperInstrumentalTools-master/Examples/Testing/UnitTests/DayShiftServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using UnitTestsTarget;
+
+namespace UnitTests
+{
+    class DayShiftServiceTests
+    {
+        [Test]
+        public void GetTomorrow()
+        {
+            // Arrange
+            var today = new DateTime(2020, 10, 06);
+            var tomorrow = new DateTime(2020, 10, 07);
+
+            var service = new DayShiftService(new DayOfWeekService());
+
+            // Act
+            var result = service.GetShiftBusinessDay(today, 1);
+
+            // Assert
+            result.Should().Be(tomorrow);
+        }
+
+        [Test]
+        public void GetMonday()
+        {
+            // Arrange
+            var friday = new DateTime(2020, 10, 09);
+            var monday = new DateTime(2020, 10, 12);
+
+            var service = new DayShiftService(new DayOfWeekService());
+
+            // Act
+            var result = service.GetShiftBusinessDay(friday, 1);
+
+            // Assert
+            result.Should().Be(monday);
+        }
+    }
+}
